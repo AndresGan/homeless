8505154f5ed6405bddbd60300f7a7b636549560d
Andres Ganan <andmgaan@misena.edu.co>
2022-02-04 18:56:15 -0500

    creamos las clases moderatehomeless y lefthomeless

--- homeless.py
+++ homeless.py
@@ -18,5 +18,26 @@ class StandardHomeless(HomeLess):
         super().__init__(name)
         
     def walk(self):
+        
         return random.choice([(1,0),(-1,0),(0,-1), (0,1)])
+    
+class ModerateHomeless(HomeLess):
+    def __init__(self, name):
+        super().__init__(name)
+        
+    def walk(self):
+        dx, dy = random.choice([(0,2),(0,-2),(2,0),(-2,0)])
+        self.x += dx
+        self.y += dy
+        return[self.x, self.y]
+    
+class leftHomeless(HomeLess):
+    def __init__(self, name):
+        super().__init__(name)
+        
+    def walk(self):
+        dx, dy = random.choice([(0,1),(0,-1),(1,0),(-5,0)])
+        self.x += dx
+        self.y += dy
+        return[self.x, self.y]
     
