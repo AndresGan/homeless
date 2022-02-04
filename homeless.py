641a622a36bfbf6ae307779f9d0ed8248d0e3c18
Andres Ganan <andmgaan@misena.edu.co>
2022-02-04 18:41:10 -0500

    modificamos la clase homeless creandole dos metodos posicion y distance_origin y en el inicializado le creamos dos parametros x,y que por defecto es 0, para que cuando inicie la clase este en el origen

--- homeless.py
+++ homeless.py
@@ -1,11 +1,18 @@
 import random
+import xxlimited
 
 
 class HomeLess:
-    def __init__(self, name):
+    def __init__(self, name, x=0, y=0):
         self.name = name
+        self.x = x
+        self.y = y
         
-        
+    def posicion (self):
+        return(self.x, self.y)
+    
+    def distance_origin(self, other_coordinate):
+        return (self.x**2 + self.y**2)**0.5           
 class StandardHomeless(HomeLess):
     def __init__(self, name):
         super().__init__(name)
